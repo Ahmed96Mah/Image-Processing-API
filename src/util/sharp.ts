c49c85699f7c9bd3c5615e55f5226e2e6cbf6532
Ahmed Abdalwahab <ahmed96mahmoud1996@gmail.com>
2022-04-19 08:27:55 +0200

    Create sharp middleware logic

---
+++ src/util/sharp.ts
@@ -0,0 +1,45 @@
+import express from 'express';
+import sharp from 'sharp';
+import path from 'path';
+
+const sharpy = (
+  req: express.Request,
+  res: express.Response,
+  next: Function
+) => {
+  // First, record the query parameters.
+  const fileName = req.query.filename;
+  const width = parseInt(req.query.width as string);
+  const height = parseInt(req.query.height as string);
+  const extension = req.query.ext;
+
+  // Call the sharp API & provide it with path to the selected image.
+  sharp(`assets/full/${fileName}.jpg`)
+    .resize(width, height)
+    .toFile(
+      `assets/thumb/thumb_${fileName}_${width}_${height}.${extension}`,
+      (err) => {
+        // This function always runs after the image is created.
+        // If there is an error (send it to the user & log it to the server).
+        if (err !== null) {
+          console.log(`error, ${err}`);
+          res.status(500).send(`error, ${err}`);
+        } else {
+          // After processing.
+          console.log(
+            `Created File: thumb_${fileName}_${width}_${height}.${extension}`
+          );
+          console.log(`Sending the processed thumb...`);
+          const dirName = path.join(__dirname, '../../');
+          res
+            .status(200)
+            .sendFile(
+              `${dirName}/assets/thumb/thumb_${fileName}_${width}_${height}.${extension}`
+            );
+          next();
+        }
+      }
+    );
+};
+
+export default sharpy;
