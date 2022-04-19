251f2d64814aca7e65f0181b4a8bb3d3d189f590
Ahmed Abdalwahab <ahmed96mahmoud1996@gmail.com>
2022-04-19 23:32:17 +0200

    Update endpoint imports for build file

--- dist/routes/api/process.js
+++ dist/routes/api/process.js
@@ -8,13 +8,13 @@ Object.defineProperty(exports, '__esModule', { value: true });
 var express_1 = __importDefault(require('express'));
 var verifier_1 = __importDefault(require('../../util/verifier'));
 var checker_1 = __importDefault(require('../../util/checker'));
-var sharp_1 = __importDefault(require('../../util/sharp'));
+var resize_1 = __importDefault(require('../../util/resize'));
 var routes = express_1.default.Router();
 routes.get(
   '/api/images',
   verifier_1.default,
   checker_1.default,
-  sharp_1.default,
+  resize_1.default,
   function (req, res) {}
 );
 exports.default = routes;
