2dfc4350ec9b999b7fd3c8a6e9be4214ed261eb4
Ahmed Abdalwahab <ahmed96mahmoud1996@gmail.com>
2022-04-20 10:37:02 +0200

    Update endpoints middlewares imports

--- src/routes/api/process.ts
+++ src/routes/api/process.ts
@@ -2,9 +2,21 @@ import express from 'express';
 import verifier from '../../util/verifier';
 import checker from '../../util/checker';
 import resize from '../../util/resize';
+import rotate from '../../util/rotate';
+import flip from '../../util/flip';
+import flop from '../../util/flop';
 
 const routes = express.Router();
 
-routes.get('/api/images', verifier, checker, resize, (req, res) => {});
+routes.get(
+  '/api/images',
+  verifier,
+  checker,
+  resize,
+  rotate,
+  flip,
+  flop,
+  (req, res) => {}
+);
 
 export default routes;
